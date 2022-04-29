4486dd87118af43ca883be3b56862a765c94b6ee
Everts Chavez <chavez.everts@gmail.com>
2022-04-29 11:12:30 -0600

    Added missing content description.

--- app/src/main/java/com/udacity/asteroidradar/domain/Asteroid.kt
+++ app/src/main/java/com/udacity/asteroidradar/domain/Asteroid.kt
@@ -12,7 +12,13 @@ data class Asteroid(
     val absoluteMagnitude: Double, val estimatedDiameter: Double,
     val relativeVelocity: Double, val distanceFromEarth: Double,
     val isPotentiallyHazardous: Boolean
-) : Parcelable
+) : Parcelable {
+    fun isPotentiallyHazardousToText(): String {
+        return if (this.isPotentiallyHazardous)
+            "The asteroid ${this.codename} is potentially hazardous"
+        else "The asteroid ${this.codename} is not hazardous"
+    }
+}
 
 
 fun List<Asteroid>.asDatabaseModel(): Array<DatabaseAsteroid> {
